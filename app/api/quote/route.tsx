9372cae51f06dd67f476be71412c2c43d32c9843
bhumilshah26 <bhumil.shah2608@gmail.com>
2025-04-12 08:51:56 +0530

    separated quotes

--- app/api/quote/route.tsx
+++ app/api/quote/route.tsx
@@ -1,19 +1,10 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
+import { quotes } from '../quotes'; 
+
 
 export const runtime = 'edge';
 
-const quotes = [
-  `Counting digits of a number was never so easy ✌️. Try: log10(n) + 1`,
-  `Want to reverse all true if's to false 😁? Try: #define if(x) if(!x)`,
-  `What is the output to this 😮? Try: printf("%d", printf("Nice Trick"))`,
-  `Min-Max of two numbers without >=< or 3rd var ✌️. Try: max = ((a + b) + abs(a - b)) / 2, min = ((a + b) - abs(a - b)) / 2`,
-  `Swapping without third variable or (>=<) operators 😁. Try: a = a + b - (b = a)`,
-  `Another swap 😮. Try: a ^= b ^= a ^= b`,
-  `Even or Odd (n & 2 == 0)🧓? (n & 1)  ? "Odd" : "Even"`,
-  `Lowercase -> Uppercase functions were difficult to remember for each P.L.✌️. Try: char ch = 'a' & '_'`,
-  `Digital root in 1 line 😮. Try: 1 + (n - 1) % 9`,
-];
 
 function getDailyIndex(): number {
   const today = new Date();
