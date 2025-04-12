a78f9c3b01f81dc3cc48446a35a3993b3c0eae3c
bhumilshah26 <bhumil.shah2608@gmail.com>
2025-04-12 08:54:01 +0530

    separated

--- app/api/quote/route.tsx
+++ app/api/quote/route.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import { quotes } from '../quotes'; 
+import { quotes } from '../../quotes'; 
 
 
 export const runtime = 'edge';
